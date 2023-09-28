4e969d60f7159bd853afa6c50b95c902af1e7537
yih365 <68409635+gigi365@users.noreply.github.com>
2023-09-28 11:03:39 -0700

    image ref

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,18 +1,26 @@
 package com.example.myapplication
 
 import android.graphics.Bitmap.CompressFormat
+import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import android.util.Base64
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet
 import com.codepath.asynchttpclient.AsyncHttpClient
+import com.codepath.asynchttpclient.RequestHeaders
 import com.codepath.asynchttpclient.RequestParams
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
 import okhttp3.Headers
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
 import java.io.ByteArrayOutputStream
 
 
@@ -27,10 +35,19 @@ class MainActivity : AppCompatActivity() {
         myDrawView.setDrawingCacheEnabled(true);
         myDrawView.id = View.generateViewId()
         myDrawView.layoutParams = ConstraintLayout.LayoutParams(
-            ConstraintLayout.LayoutParams.MATCH_PARENT,
-            350
+            512,
+           512
         )
+        val border = GradientDrawable()
+        border.setColor(-0x1) //white background
+        border.setStroke(1, -0x1000000) //black border with full opacity
+        myDrawView.background = border
         parent.addView(myDrawView)
+
+        // Add view for image ref
+        val imageRefView = ImageView(this)
+        imageRefView.id = View.generateViewId()
+        parent.addView(imageRefView)
 
         // Add button
         val button = Button(this)
@@ -42,18 +59,26 @@ class MainActivity : AppCompatActivity() {
             val byteArrayOutputStream = ByteArrayOutputStream()
             myDrawView.drawingCache.compress(CompressFormat.PNG, 100, byteArrayOutputStream)
             val byteArray: ByteArray = byteArrayOutputStream.toByteArray()
-            val encoded: String = Base64.encodeToString(byteArray, Base64.DEFAULT)
-            Log.d(TAG, encoded)
+
+            val n = 3
+            val body: RequestBody = MultipartBody.Builder()
+                .setType(MultipartBody.FORM)
+                .addFormDataPart("image", "drawing.png", byteArray.toRequestBody("image/png".toMediaTypeOrNull()))
+                .addFormDataPart("n", n.toString())
+                .addFormDataPart("size", "512x512")
+                .build()
 
             // Make api call
-            val PROJECT_ID = "draw-caption-ref"
-            val URL = "https://us-central1-aiplatform.googleapis.com/v1/projects/${PROJECT_ID}/locations/us-central1/publishers/google/models/imagetext:predict"
+            val URL = "https://api.openai.com/v1/images/variations"
             val client = AsyncHttpClient()
             val params = RequestParams()
-            params["sampleCount"] = "1"
-            params["image"] = encoded
-//            params["language"] = "en"
-            client.get(URL, params, object: JsonHttpResponseHandler() {
+            val requestHeaders = RequestHeaders()
+            // TODO: hide this
+            val API_KEY = "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"
+            requestHeaders["Authorization"] = "Bearer $API_KEY"
+            requestHeaders["Content-Type"] = "multipart/form-data"
+
+            client.post(URL, requestHeaders, params, body, object: JsonHttpResponseHandler() {
                 override fun onFailure(
                     statusCode: Int,
                     headers: Headers?,
@@ -67,8 +92,22 @@ class MainActivity : AppCompatActivity() {
                 }
 
                 override fun onSuccess(statusCode: Int, headers: Headers?, json: JSON?) {
-                    Log.d(TAG, "success")
                     Log.d(TAG, json.toString())
+
+                    val url = json?.jsonObject?.getJSONArray("data")?.getJSONObject(0)?.getString("url")
+                    Log.d(TAG, "url is $url")
+
+                    var i = 0
+                    imageRefView.setOnClickListener{
+                        val clickUrl = json?.jsonObject?.getJSONArray("data")?.getJSONObject(i)?.getString("url")
+                        Glide.with(this@MainActivity).load(clickUrl).fitCenter().override(512).into(imageRefView)
+                        i += 1
+                        if (i==n) {
+                            i = 0
+                        }
+                    }
+
+                    Glide.with(this@MainActivity).load(url).fitCenter().override(512).into(imageRefView)
                 }
             })
 //            var fos: FileOutputStream? = null
@@ -81,11 +120,6 @@ class MainActivity : AppCompatActivity() {
 //            myDrawView.drawingCache.compress(CompressFormat.PNG, 95, fos)
         }
         parent.addView(button)
-
-//        // Add text
-//        val captionText = TextView(this)
-//        captionText.id = View.generateViewId()
-//
 
         // Set position of view
         val constraintSet = ConstraintSet()
@@ -101,11 +135,19 @@ class MainActivity : AppCompatActivity() {
             myDrawView.id, ConstraintSet.TOP,
             button.id, ConstraintSet.BOTTOM, 0
         )
+        constraintSet.connect(
+            imageRefView.id, ConstraintSet.LEFT,
+            button.id, ConstraintSet.RIGHT, 10
+        )
+        constraintSet.connect(
+            imageRefView.id, ConstraintSet.RIGHT,
+            parent.id, ConstraintSet.RIGHT, 10
+        )
+        constraintSet.connect(
+            imageRefView.id, ConstraintSet.TOP,
+            parent.id, ConstraintSet.TOP, 10
+        )
         constraintSet.applyTo(parent)
-    }
-
-    private fun getFileName(): String {
-        return "drawingFile.png"
     }
 
     companion object {
